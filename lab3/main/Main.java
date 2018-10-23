10e259495b947a18a14ca8b62f91c6b597c4d3f7
matski98 <mat_ski@op.pl>
2018-10-23 23:12:41 +0200

    dodanie zadania

--- lab3/main/Main.java
+++ lab3/main/Main.java
@@ -3,11 +3,15 @@ import klasy.*;
 public class Main {
 
     public static void main(String[] args) {
-        EmailMessage wiadomosc = EmailMessage.builder()
-                .addFrom("sbobek@agh.edu.pl")
-                .addTo("student@agh.edu.pl")
-                .addSubject("Mail testowy")
-                .addcontent("Brak tresci")
-                .build();
+        try {
+            EmailMessage wiadomosc = EmailMessage.builder()
+                    .addFrom("sbobek@agh.edu.pl")
+                    .addTo("student@agh.edu.pl")
+                    .addSubject("Mail testowy")
+                    .addcontent("Brak tresci")
+                    .build();
+            wiadomosc.send();
+        }
+        catch (Exception e){ e.printStackTrace(); }
     }
 }
